1858d707599db0945837c313e9ae276d83e00ac6
Zora Crimson <katharinamaldon@gmail.com>
2018-11-02 17:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function add(a,b)  {
+function add(a, b)  {
   return a + b 
 }
-function subtract(a,b)  {
+function subtract(a, b)  {
   return a - b
 }
-function multiply(a,b)  {
+function multiply(a, b)  {
   return a * b
 }
-function divide(a,b)
+function divide(a, b)  {
+  return a / b
+}
+function
